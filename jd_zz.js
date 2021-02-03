d72e2091b30cce5a950da2695b3fcbf1da9b1d14
hajiuhajiu <2705322732@qq.com>
2021-02-03 16:18:02 +0800

    同步作者更新

--- jd_zz.js
+++ jd_zz.js
@@ -51,8 +51,8 @@ const inviteCodes = [
 !(async () => {
   $.tuanList = []
   await requireConfig();
-  if (helpAuthor) await getAuthorShareCode('https://gitee.com/shuye72/updateTeam/raw/master/jd_zz.json');
-  if (helpAuthor) await getAuthorShareCode('https://gitee.com/shuye72/updateTeam/raw/master/jd_zz.json');
+  if (helpAuthor) await getAuthorShareCode('https://raw.githubusercontent.com/hajiuhajiu/jdsign1112/master/backUp/zztuan.json');
+  if (helpAuthor) await getAuthorShareCode('https://raw.githubusercontent.com/hajiuhajiu/jdsign1112/master/backUp/zztuan.json');
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
@@ -152,7 +152,6 @@ function getAuthorShareCode(url) {
         } else {
           $.tuanList = $.tuanList.concat(JSON.parse(data))
           console.log(`作者助力码获取成功`)
-	  console.log(tuanlist)	
         }
       } catch (e) {
         $.logErr(e, resp)
@@ -234,8 +233,6 @@ function openTuan() {
         } else {
           if (safeGet(data)) {
             data = JSON.parse(data);
-			 console.log(data);
-		          console.log(body);
             if (data['success']) {
               $.hasOpen = true
             }
