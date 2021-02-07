fea50bb66c8a6bfbcde371005c484718ca74e74d
hajiuhajiu <2705322732@qq.com>
2021-02-08 05:43:53 +0800

    Update jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -8,7 +8,6 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes = ['']
-  '
 // 判断github action里面是否有京喜工厂互助码
 if (process.env.DREAM_FACTORY_SHARE_CODES) {
   if (process.env.DREAM_FACTORY_SHARE_CODES.indexOf('&') > -1) {
