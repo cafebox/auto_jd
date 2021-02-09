20ffd475f8df476364aec0520ad67dcad599407c
hajiuhajiu <2705322732@qq.com>
2021-02-09 18:58:09 +0800

    Update jd_jxfactory.js

--- jd_jxfactory.js
+++ jd_jxfactory.js
@@ -37,12 +37,10 @@ const JD_API_HOST = 'https://m.jingxi.com';
 const notify = $.isNode() ? require('./sendNotify') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 const randomCount = $.isNode() ? 20 : 5;
-let tuanActiveId = `6S9y4sJUfA2vPQP6TLdVIQ==11`;
+let tuanActiveId = `TvjO5k4gaVqVHMRJIogd_g==`;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '';
-const inviteCodes = [
-   '-OvElM=',
-  'GF'
+const inviteCodes = ['N8QdMHreY_pd7rIHq9zlFw==@LAxdowQnMU3ox65OdwNwZA==7UFJoyEFc-k42Yrnh_-gEA=='
 ];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 $.tuanIds = [];
