5519719b838f2387f4f6f8c58172294f71fdf5c3
hajiuhajiu <2705322732@qq.com>
2021-02-19 10:53:59 +0800

    Update jd_zzupdate.js

--- jd_zzupdate.js
+++ jd_zzupdate.js
@@ -28,7 +28,8 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
     return;
   }
-  for (let i = 0; i < cookiesArr.length; i++) {
+  //for (let i = 0; i < cookiesArr.length; i++) {
+  for (let i = 0; i < 5; i++) { 
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
       $.UserName = decodeURIComponent(cookie.match(/pt_pin=(.+?);/) && cookie.match(/pt_pin=(.+?);/)[1])
