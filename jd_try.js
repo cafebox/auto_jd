23be79fd90cec902b839d7f24b668b13e4846b4b
hajiuhajiu <2705322732@qq.com>
2021-03-03 05:36:10 +0800

    去除日常通知

--- jd_try.js
+++ jd_try.js
@@ -430,7 +430,7 @@ async function showMsg() {
 			"open-url": 'https://try.m.jd.com/user'
 		})
 		if($.isNode()){
-			await notify.sendNotify(`${$.name} - 账号${$.index} - ${$.nickName}`, message)
+			//await notify.sendNotify(`${$.name} - 账号${$.index} - ${$.nickName}`, message)
 		}
 	} else {
 		console.log(message)
