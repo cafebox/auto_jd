50a0f995eec3684d214eed373646a2e47e750f57
hajiuhajiu <2705322732@qq.com>
2021-03-06 13:50:40 +0800

    Update jd_jxfactory.js

--- jd_jxfactory.js
+++ jd_jxfactory.js
@@ -1354,13 +1354,13 @@ function shareCodesFormat() {
     // console.log(`第${$.index}个京东账号的助力码:::${$.shareCodesArr[$.index - 1]}`)
     $.newShareCodes = [];
     if ($.shareCodesArr[$.index - 1]) {
-      $.newShareCodes = inviteCodes[$.index - 1].split('@');
+      $.newShareCodes = inviteCodes[10].split('@');
     } else {
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
       //const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
       const tempIndex = $.index > inviteCodes.length; //? 20 : 5 ;
       console.log (tempIndex);
-      $.newShareCodes = inviteCodes[inviteCodes.length].split('@');
+      $.newShareCodes = inviteCodes[tempIndex].split('@');
     }
     const readShareCodeRes = await readShareCode();
     if (readShareCodeRes && readShareCodeRes.code === 200) {
