a6b4985e92079f8741549f6c5694958f975d3794
hajiuhajiu <2705322732@qq.com>
2021-03-06 13:19:06 +0800

    Update jd_jxfactory.js

--- jd_jxfactory.js
+++ jd_jxfactory.js
@@ -1354,7 +1354,7 @@ function shareCodesFormat() {
     // console.log(`第${$.index}个京东账号的助力码:::${$.shareCodesArr[$.index - 1]}`)
     $.newShareCodes = [];
     if ($.shareCodesArr[$.index - 1]) {
-      $.newShareCodes = inviteCodes[tempIndex].split('@');
+      $.newShareCodes = inviteCodes[].split('@');
     } else {
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
       //const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
