0d3152b03211fce3f65398f9a772e4c563ffc12e
hajiuhajiu <2705322732@qq.com>
2021-03-07 15:20:31 +0800

    Update jdPetShareCodes.js

--- jdPetShareCodes.js
+++ jdPetShareCodes.js
@@ -8,8 +8,7 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let PetShareCodes = [
-  'MTAxODc2NTEzNTAwMDAwMDAwMjg3MDg2MA==@MTAxODc2NTEzMzAwMDAwMDAyNzUwMDA4MQ==@MTAxODc2NTEzMjAwMDAwMDAzMDI3MTMyOQ==@MTAxODc2NTEzNDAwMDAwMDAzMDI2MDI4MQ==',//账号一的好友shareCode,不同好友中间用@符号隔开
-  'MTAxODc2NTEzMjAwMDAwMDAzMDI3MTMyOQ==@MTAxODcxOTI2NTAwMDAwMDAyNjA4ODQyMQ==@MTAxODc2NTEzOTAwMDAwMDAyNzE2MDY2NQ==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  "MTAxODc2NTE0NzAwMDAwMDAwMDE2ODA1MQ==&MTAxODc2NTEzNTAwMDAwMDAwODI1Nzg3Nw==&MTAxODc2NTE0NzAwMDAwMDAwMDI5NDkwOQ==&MTAxODc2NTEzMTAwMDAwMDAwMDI5NDg5MQ==&MTAxODcxMjg4ODAwMDAwMDAwMDI5NTExMQ==&MTAxODc2NTE0NzAwMDAwMDAwMDI5NTA2OQ==&MTE1NDQ5MzYwMDAwMDAwMzY4NzY2MzE=&MTAxODc2NTE0NzAwMDAwMDAwMDI5NDkzNQ=="
 ]
 // 判断github action里面是否有东东萌宠互助码
 if (process.env.PETSHARECODES) {
