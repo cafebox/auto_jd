065658054411739674c241d911ff7b219ec69d4e
hajiuhajiu <2705322732@qq.com>
2021-03-16 13:51:29 +0800

    Update jd_jxfactory.js

--- jd_jxfactory.js
+++ jd_jxfactory.js
@@ -99,7 +99,7 @@ async function jdDreamFactory() {
     }
     await userInfo();
     await QueryFriendList();//查询今日招工情况以及剩余助力次数
-    // await joinLeaderTuan();//参团
+    await joinLeaderTuan();//参团
     await helpFriends();
     if (!$.unActive) return
     await getUserElectricity();
