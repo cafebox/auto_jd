5f8aa11a6a9f10ba8e2b2f002d81969547466a89
hajiuhajiu <2705322732@qq.com>
2021-03-19 11:22:35 +0800

    Update jd_jxnc_s.js

--- jd_jxnc_s.js
+++ jd_jxnc_s.js
@@ -37,7 +37,7 @@ let tokenNull = {'farm_jstoken': '', 'phoneid': '', 'timestamp': ''}; // 内置�
 let tokenArr = []; // 用户 token 数组
 let currentToken = {}; // 当前用户 token//@;
 let shareCode = '{\"smp\":\"4c092872f737bfd225c1567e4d64261d",\"active\":\"jdnc_1_2yuanhamigua210315_2\",\"joinnum\":1}&{\"smp\":\"ba9a86a0e257f56656c6fa20cff1fb05",\"active\":\"jdnc_1_caomi210305_2\",\"joinnum\":1}'; // 内置助力码
-let jxncShareCodeArr = ["{\"smp\":\"dc82f144a5e12e72dda16d5240154990",\"active\":\"jdnc_1_cphuasheng210305_2\",\"joinnum\":1},{\"smp\":\"3700043571dc118e7178928b60859389\",\"active\":\"jdnc_1_cphuasheng210305_2\",\"joinnum\":1},{\"smp\":\"3700043571dc118e7178928b60859389\",\"active\":\"jdnc_1_cphuasheng210305_2\",\"joinnum\":1}"]; // 用户 助力码 数组let currentShareCode = []; // 当前用户 要助力的助力码
+let jxncShareCodeArr = [{\"smp\":\"dc82f144a5e12e72dda16d5240154990",\"active\":\"jdnc_1_cphuasheng210305_2\",\"joinnum\":1},{\"smp\":\"3700043571dc118e7178928b60859389\",\"active\":\"jdnc_1_cphuasheng210305_2\",\"joinnum\":1},{\"smp\":\"3700043571dc118e7178928b60859389\",\"active\":\"jdnc_1_cphuasheng210305_2\",\"joinnum\":1}]; // 用户 助力码 数组let currentShareCode = []; // 当前用户 要助力的助力码
 const openUrl = `openjd://virtual?params=${encodeURIComponent('{ "category": "jump", "des": "m", "url": "https://wqsh.jd.com/sns/201912/12/jxnc/detail.html?ptag=7155.9.32&smp=b47f4790d7b2a024e75279f55f6249b9&active=jdnc_1_chelizi1205_2"}',)}`; // 打开京喜农场
 let subTitle = '', message = '', option = {'open-url': openUrl}; // 消息副标题，消息正文，消息扩展参数
 const JXNC_API_HOST = 'https://wq.jd.com/';
