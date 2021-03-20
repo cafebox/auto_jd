7e0ced636beb202c37570ec860eb606ded23af53
hajiuhajiu <2705322732@qq.com>
2021-03-21 07:17:12 +0800

    Add files via upload

--- jd_fruit.js
+++ jd_fruit.js
@@ -1264,7 +1264,7 @@ function shareCodesFormat() {
     } else {
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
       const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
-      newShareCodes = shareCodes[tempIndex].split('@');
+      //newShareCodes = shareCodes[tempIndex].split('@');
     }
     const readShareCodeRes = await readShareCode();
     if (readShareCodeRes && readShareCodeRes.code === 200) {
