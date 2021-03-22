1b839f34a1932655aa7ba9cbe1479791a9e3e96c
hajiuhajiu <2705322732@qq.com>
2021-03-22 13:47:02 +0800

    Update jd_jxfactory.js

--- jd_jxfactory.js
+++ jd_jxfactory.js
@@ -1321,9 +1321,13 @@ function readShareCode() {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
           console.log(`${$.name} API请求失败，请检查网路重试`)
+          console.log(`随机取助力码benyong互助码后面(不影响已有固定互助)`)
+          $.get({url: `https://raw.githubusercontent.com/hajiuhajiu/jdsign1112/master/backUp/total/jxfactory.json`, 'timeout': 10000},(err, resp, data)=>{    
+            data = JSON.parse(data);})
+
         } else {
           if (data) {
-            console.log(`随机取助力码放到您固定的互助码后333333333333333333面(不影响已有固定互助)`)
+            console.log(`随机取11(不影响已有固定互助)`)
             data = JSON.parse(data);
           }
         }
