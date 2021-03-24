fbd195530bf0e9d0fd9b03f216648e17b54bc429
hajiuhajiu <2705322732@qq.com>
2021-03-25 04:56:52 +0800

    Update jd_jxFactoryCreateTuan.js

--- jd_jxFactoryCreateTuan.js
+++ jd_jxFactoryCreateTuan.js
@@ -7,7 +7,7 @@ const JD_API_HOST = 'https://m.jingxi.com';
 const fs = require('fs');
 const notify = $.isNode() ? require('./sendNotify') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
-const tuanActiveId = `KfUjD48K74J2JAwvQSyoDw==`;
+const tuanActiveId = `i9ideMF_BUOdVtmbe1pSeA==`;
 let cookiesArr = [], cookie = '', message = '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 Object.keys(jdCookieNode).forEach((item) => {
