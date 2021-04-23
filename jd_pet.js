418f69a9c6a07f310a668cd5ba4203fe110e6d6f
hajiuhajiu <2705322732@qq.com>
2021-04-23 11:19:08 +0800

    更新助力， 不影响运行， 忽略

--- jd_pet.js
+++ jd_pet.js
@@ -448,7 +448,7 @@ async function showMsg() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: "https://raw.githubusercontent.com/hajiuhajiu/jdsign1112/master/backUp/total/jd_pet.json",headers:{
+    $.get({url: "https://gitee.com/xr2021/share/raw/master/jd_pet.json",headers:{
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }}, async (err, resp, data) => {
       try {
