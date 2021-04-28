a1d27146099080e6ba73e3a862648fb64feffff6
hajiuhajiu <2705322732@qq.com>
2021-04-28 14:32:20 +0800

    Update jd_pet.js

--- jd_pet.js
+++ jd_pet.js
@@ -450,7 +450,7 @@ async function showMsg() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: "https://gitee.com/xr2021/share/blob/master/jd_pet.json",headers:{
+    $.get({url: "https://gitee.com/xr2021/share/raw/master/jd_pet.json",headers:{
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }}, async (err, resp, data) => {
       try {
@@ -458,7 +458,7 @@ function readShareCode() {
           console.log(`${JSON.stringify(err)}`)
           console.log(`${$.name} API请求失败，将切换为备用API`)
           console.log(`随机取助力码放到您固定的互助码后面(不影响已有固定互助)`)
-          $.get({url: `https://gitee.com/xr2021/share/blob/master/jd_pet.json`, 'timeout': 10000},(err, resp, data)=>{
+          $.get({url: `https://gitee.com/xr2021/share/raw/master/jd_pet.json`, 'timeout': 10000},(err, resp, data)=>{
           data = JSON.parse(data);})
         } else {
           if (data) {
