d0cb692ae318b74fb85aaf660664c10ded97a2e0
hajiuhajiu <2705322732@qq.com>
2021-05-20 02:40:24 +0800

    Update jd_jxFactoryCreateTuan.js

--- jd_jxFactoryCreateTuan.js
+++ jd_jxFactoryCreateTuan.js
@@ -2,7 +2,7 @@
 京东京喜工厂自动开团
  */
 const $ = new Env('京东京喜工厂自动开团');
-const tuanActiveId = `mNGPTrOPluOSeZcupPI40w==`;
+const tuanActiveId = `XBYi-5lVdNspWhlNwhpE6Q==`;
 const JD_API_HOST = 'https://m.jingxi.com';
 const fs = require('fs');
 const notify = $.isNode() ? require('./sendNotify') : '';
